5acd77a3504a31e120a61bae42c857230acbaac4
Gustavo Henrique <gustavo@gustavohenrique.net>
2023-05-28 14:19:59 -0300

    Fix layout and build script

--- generator/resources/build.sh
+++ generator/resources/build.sh
@@ -12,9 +12,9 @@ NOW=$(date +"%Y%m%d_%H%M")
 
 echo "Building ${NOW} ${VERSION}"
 
-GOARCH="${ARCH}"
-GOOS="${OS}"
-GO111MODULE=on
+export GOARCH="${ARCH}"
+export GOOS="${OS}"
+export GO111MODULE=on
 APP_NAME=$(go list -m)
 
 CGO_ENABLED=0 go build \
